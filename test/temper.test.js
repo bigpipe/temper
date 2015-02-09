e0b40912b594140f9ad2fd6f95efd62842edb37d
Arnout Kazemier <info@3rd-Eden.com>
2015-02-09 14:28:51 +0100

    [deps] Use assume

--- test/temper.test.js
+++ test/temper.test.js
@@ -2,8 +2,7 @@ describe('temper', function () {
   'use strict';
 
   var Temper = require('../')
-    , chai = require('chai')
-    , expect = chai.expect
+    , assume = require('assume')
     , temper;
 
   beforeEach(function () {
@@ -15,40 +14,40 @@ describe('temper', function () {
   });
 
   it('has list with supported types', function () {
-    expect(temper.supported).to.have.property('.ejs');
-    expect(temper.supported).to.have.property('.jade');
-    expect(temper.supported).to.have.property('.mustache');
-    expect(temper.supported).to.have.property('.hbs');
-    expect(temper.supported).to.have.property('.handlebars');
+    assume(temper.supported).to.have.property('.ejs');
+    assume(temper.supported).to.have.property('.jade');
+    assume(temper.supported).to.have.property('.mustache');
+    assume(temper.supported).to.have.property('.hbs');
+    assume(temper.supported).to.have.property('.handlebars');
 
-    expect(temper.supported['.ejs']).to.include('ejs');
-    expect(temper.supported['.jade']).to.include('jade');
-    expect(temper.supported['.mustache']).to.include('hogan.js');
-    expect(temper.supported['.mustache']).to.include('mustache');
-    expect(temper.supported['.mustache']).to.include('handlebars');
-    expect(temper.supported['.hbs']).to.include('handlebars');
-    expect(temper.supported['.handlebars']).to.include('handlebars');
-    expect(temper.supported['.html']).to.include('html');
+    assume(temper.supported['.ejs']).to.include('ejs');
+    assume(temper.supported['.jade']).to.include('jade');
+    assume(temper.supported['.mustache']).to.include('hogan.js');
+    assume(temper.supported['.mustache']).to.include('mustache');
+    assume(temper.supported['.mustache']).to.include('handlebars');
+    assume(temper.supported['.hbs']).to.include('handlebars');
+    assume(temper.supported['.handlebars']).to.include('handlebars');
+    assume(temper.supported['.html']).to.include('html');
   });
 
   describe('#read', function () {
     it('should read the file contents and return a string', function () {
       var data = temper.read(__dirname +'/fixtures/template.jade');
 
-      expect(data).to.be.a('string');
-      expect(data).to.contain('doctype 5');
+      assume(data).is.a('string');
+      assume(data).to.contain('doctype 5');
     });
 
     it('should cache the file lookup', function () {
       var data = temper.read(__dirname +'/fixtures/template.jade');
-      expect(data).to.be.a('string');
-      expect(data).to.contain('doctype 5');
+      assume(data).is.a('string');
+      assume(data).to.contain('doctype 5');
 
-      expect(temper.file).to.have.property(__dirname +'/fixtures/template.jade');
+      assume(temper.file).to.have.property(__dirname +'/fixtures/template.jade');
 
       data = temper.read(__dirname +'/fixtures/template.jade');
-      expect(data).to.be.a('string');
-      expect(data).to.contain('doctype 5');
+      assume(data).is.a('string');
+      assume(data).to.contain('doctype 5');
     });
   });
 
@@ -56,29 +55,29 @@ describe('temper', function () {
     it('requires the module and returns it', function () {
       var jade = temper.require('jade');
 
-      expect(jade).to.equal(require('jade'));
+      assume(jade).equals(require('jade'));
     });
 
     it('caches the module', function () {
       var jade = temper.require('jade');
 
-      expect(jade).to.equal(require('jade'));
-      expect(temper.required.jade).to.equal(require('jade'));
+      assume(jade).equals(require('jade'));
+      assume(temper.required.jade).equals(require('jade'));
     });
 
     it('registers engine `null` for HTML', function () {
       var html = temper.require('html');
 
-      expect(html).to.equal(null);
-      expect(temper.required.html).to.equal(null);
+      assume(html).equals(null);
+      assume(temper.required.html).equals(null);
     });
 
     it('throws an error if a module doesnt exist', function () {
       try { temper.require('cowdoodlesack'); }
       catch (e) {
-        expect(e).to.be.instanceOf(Error);
-        expect(e.message).to.include('npm install');
-        return expect(e.message).to.include('cowdoodlesack');
+        assume(e).to.be.instanceOf(Error);
+        assume(e.message).to.include('npm install');
+        return assume(e.message).to.include('cowdoodlesack');
       }
 
       throw new Error('I should have failed');
@@ -89,19 +88,19 @@ describe('temper', function () {
     it('should throw an error for a unknown extension', function () {
       try { temper.discover('/care/bear/path/template.trololol'); }
       catch (e) {
-        expect(e).to.be.instanceOf(Error);
-        expect(e.message).to.include('trololol');
-        return expect(e.message).to.include('Unknown file extension');
+        assume(e).to.be.instanceOf(Error);
+        assume(e.message).to.include('trololol');
+        return assume(e.message).to.include('Unknown file extension');
       }
 
       throw new Error('I should have failed');
     });
 
     it('returns the correct engine', function () {
-      expect(temper.discover('foo.jade')).to.equal('jade');
-      expect(temper.discover('foo.ejs')).to.equal('ejs');
-      expect(temper.discover('foo.mustache')).to.equal('hogan.js');
-      expect(temper.discover('foo.html')).to.equal('html');
+      assume(temper.discover('foo.jade')).equals('jade');
+      assume(temper.discover('foo.ejs')).equals('ejs');
+      assume(temper.discover('foo.mustache')).equals('hogan.js');
+      assume(temper.discover('foo.html')).equals('html');
     });
   });
 
@@ -109,29 +108,29 @@ describe('temper', function () {
     it('compiles a jade template', function () {
       var obj = temper.compile('h1 hello', 'jade');
 
-      expect(obj.client).to.be.a('string');
-      expect(obj.library).to.be.a('string');
-      expect(obj.server).to.be.a('function');
-      expect(obj.server()).to.equal('<h1>hello</h1>');
-      expect(obj.hash).to.be.a('object');
-      expect(obj.hash.client).to.be.a('string');
-      expect(obj.hash.server).to.be.a('string');
-      expect(obj.hash.library).to.be.a('string');
+      assume(obj.client).is.a('string');
+      assume(obj.library).is.a('string');
+      assume(obj.server).is.a('function');
+      assume(obj.server()).equals('<h1>hello</h1>');
+      assume(obj.hash).is.a('object');
+      assume(obj.hash.client).is.a('string');
+      assume(obj.hash.server).is.a('string');
+      assume(obj.hash.library).is.a('string');
     });
 
     describe('.html', function () {
       it('returns surrogate compiler for HTML', function () {
         var obj = temper.compile('<h1>regular</h1>', 'html');
 
-        expect(obj.client).to.be.a('string');
-        expect(obj.library).to.be.a('string');
-        expect(obj.library).to.equal('');
-        expect(obj.server).to.be.a('function');
-        expect(obj.server()).to.equal('<h1>regular</h1>');
+        assume(obj.client).is.a('string');
+        assume(obj.library).is.a('string');
+        assume(obj.library).equals('');
+        assume(obj.server).is.a('function');
+        assume(obj.server()).equals('<h1>regular</h1>');
 
         var client = (new Function('return '+ obj.client))();
-        expect(client).to.be.a('function');
-        expect(client()).to.equal('<h1>regular</h1>');
+        assume(client).is.a('function');
+        assume(client()).equals('<h1>regular</h1>');
       });
 
       it('supports basic replacements of data', function () {
@@ -139,8 +138,8 @@ describe('temper', function () {
 
         obj.client = (new Function('return '+ obj.client))();
 
-        expect(obj.server({ key: 'bar' })).to.equal('<h1>bar</h1>');
-        expect(obj.client({ key: 'bar' })).to.equal('<h1>bar</h1>');
+        assume(obj.server({ key: 'bar' })).equals('<h1>bar</h1>');
+        assume(obj.client({ key: 'bar' })).equals('<h1>bar</h1>');
       });
 
       it('doesnt die when supplied with an Object.create(null)', function () {
@@ -150,8 +149,8 @@ describe('temper', function () {
         obj.client = (new Function('return '+ obj.client))();
 
         data.key = 'bar';
-        expect(obj.server(data)).to.equal('<h1>bar</h1>');
-        expect(obj.client(data)).to.equal('<h1>bar</h1>');
+        assume(obj.server(data)).equals('<h1>bar</h1>');
+        assume(obj.client(data)).equals('<h1>bar</h1>');
       });
     });
   });
@@ -160,13 +159,13 @@ describe('temper', function () {
     it('handles filenames without special characters', function() {
       var name = temper.normalizeName('/home/templates/template.jade');
 
-      expect(name).to.equal('template');
+      assume(name).equals('template');
     });
 
     it('handles filenames with special characters in filenames', function() {
       var name = temper.normalizeName('/home/templates/09$-money_$00-test.jade');
 
-      expect(name).to.equal('$money_$00test');
+      assume(name).equals('$money_$00test');
     });
 
   });
